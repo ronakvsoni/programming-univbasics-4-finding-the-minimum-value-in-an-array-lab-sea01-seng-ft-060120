386c0cf161fded2304f03b7ff4cd684886fd4eb6
Ronak V Soni <ronakvsoni@gmail.com>
2020-07-05 21:29:44 +0000

    Done.

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,15 @@
+z = [10,5,3,7,19,1,3,10,-1]
 def find_min_value(array)
+  minvalue = 10000000000
+  index = 0
+  while index < array.length do
+    if minvalue > array[index]
+      minvalue = array[index]
+    end
+      index = index + 1
+    end
+      minvalue
+  end
   # Add your solution here
-end
+
+puts find_min_value(z)
